a245e945bc3a7cc1f6bdc31c4f016295bb7f1ced
Leoces Martinez <leonelmartinez@bisacorporation.com>
2025-06-23 17:42:00 -0500

    primer commit, cración del proyecto, en búsqueda de la felicidad

---
+++ src/main/java/com/finanzas/finanzas_personales/FinanzasPersonalesApplication.java
@@ -0,0 +1,13 @@
+package com.finanzas.finanzas_personales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzasPersonalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzasPersonalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finanzas/finanzas_personales/FinanzasPersonalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzas.finanzas_personales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasPersonalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
